4e758114630024b89515ea584c81bb4344fae0cf
abcmPrivate <MoekoEbisawa@gmail.com>
2019-11-13 09:10:09 +0900

    OGP画像出すサンプル

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.bigben = functions.https.onRequest((req, res) => {
+
+  const SITEURL = "〇〇〇〇〇"
+  const TITLE = "〇〇〇〇〇"
+  const DESCRIPTION = "〇〇〇〇〇"
+  const IMAGE = `https://firebasestorage.googleapis.com/v0/b/tapistagram.appspot.com/o/test.jpg?alt=media&token=6350d7f5-7897-461a-b40a-32128d576c37`
+
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+      <meta property="og:title" content="${TITLE}">
+      <meta property="og:image" content="${IMAGE}">
+      <meta property="og:description" content="${DESCRIPTION}">
+      <meta property="og:url" content="${SITEURL}">
+      <meta property="og:type" content="website">
+      <meta property="og:site_name" content="${TITLE}">
+      <meta name="twitter:site" content="〇〇〇〇〇〇">
+      <meta name="twitter:card" content="summary_large_image">
+      <meta name="twitter:title" content="${TITLE}">
+      <meta name="twitter:image" content="${IMAGE}">
+      <meta name="twitter:description" content="${DESCRIPTION}">
+    </head>
+    <body>
+      ${'BONG '.repeat(3)}
+    </body>
+  </html>`);
+});
