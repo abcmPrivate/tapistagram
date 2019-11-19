9a685328e342b550dc32e1894877388913b0d30f
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-11-19 09:07:23 +0900

    ロゴ作った

---
+++ apis/material.js
@@ -0,0 +1,118 @@
+const material = {
+    tapioca: [
+        {
+            name: 'blacksugar',
+            name_jp: '黒糖',
+            color: '#', // color code
+        },
+        {
+            name: 'whitesugar',
+            name_jp: '和三盆',
+            color: '#fff'
+        },
+        {
+            name: 'greentea',
+            name_jp: '抹茶',
+            color: '#'
+        },
+        {
+            name: 'strawberry',
+            name_jp: 'いちご',
+            color: '#'
+        },
+        {
+            name: 'tea',
+            name_jp: '紅茶',
+            color: '#'
+        }
+    ],
+    source: [
+        {
+            name: 'blacksugar',
+            name_jp: '黒糖',
+            color: '#'
+        },
+        {
+            name: 'greentea',
+            name_jp: '抹茶',
+            color: '#'
+        },
+        {
+            name: 'strawberry',
+            name_jp: 'いちご',
+            color: '#'
+        }
+    ],
+    straw: [
+        '#', '#', '#', '#'
+    ],
+    drink: [
+        {
+            name: 'milktea',
+            name_jp: 'ミルクティー',
+            color: '#'
+        },
+        {
+            name: 'milk',
+            name_jp: 'ミルク',
+            color: '#'
+        },
+        {
+            name: 'oolongtea',
+            name_jp: 'ウーロンティー',
+            color: '#'
+        },
+        {
+            name: 'tea',
+            name_jp: '紅茶',
+            color: '#'
+        },
+        {
+            name: 'jasminetea',
+            name_jp: 'ジャスミンティー',
+            color: '#'
+        },
+        {
+            name: 'greentea',
+            name_jp: '抹茶',
+            color: '#'
+        },
+        {
+            name: 'greentea-milk',
+            name_jp: '抹茶ミルク',
+            color: '#'
+        },
+        {
+            name: 'roasted-greentea',
+            name_jp: 'ほうじ茶',
+            color: '#'
+        },
+        {
+            name: 'roasted-greentea',
+            name_jp: 'ほうじ茶ミルク',
+            color: '#'
+        },
+    ],
+    foam: [
+        {
+            name: 'milk',
+            name_jp: 'ミルクフォーム',
+        },
+        {
+            name: 'cheese',
+            name_jp: 'チーズフォーム',
+        },
+        {
+            name: 'cookie-cream',
+            name_jp: '',
+        },
+    ],
+    mark: [
+        {
+            name: 'kongcha',
+            name_jp: 'こんちゃ',
+            color: '#'
+        }
+    ]
+}
+export default material
--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,19 +1,26 @@
 <template>
     <header class="header">
-        <p class="header-logo">tapistagram</p>
+        <p class="header-logo"><Logo /></p>
     </header>
 </template>
-
+<script>
+import Logo from '@/components/common/Logo'
+export default {
+    components: {
+        Logo
+    }
+}
+</script>
 <style lang="scss" scoped>
 .header {
     position: sticky;
     background-color: $color-white;
     border-bottom: 1px solid $color-border;
     padding: 20px 10px;
-    text-align: center;
+    display: flex;
+    justify-content: center;
     &-logo {
-        color: $color-milktea;
-        font-weight: bold;
+        width: 120px;
     }
 }
 </style>
