021aba7822c367cac09d530d031e7ecdabc5ca37
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-12-05 09:47:54 +0900

    cloud func

--- functions/index.js
+++ functions/index.js
@@ -8,42 +8,40 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 const db = admin.firestore()
 
-const site_name = 'tapistagram'
-const title = 'tapistagram'
-const meta_description = 'タピオカをつくるやつ'
-const meta_keywords = ['タピオカ']
-const og_description = 'タピオカつくるやつです'
-const og_image_width = 1200
-const og_image_height = 630
-const fb_appid = ''
-const tw_description = 'タピオカつくるやつです'
-const tw_site = ''
-const tw_creator = ''
+const SITE_NAME = 'tapistagram'
+const TITLE = 'tapistagram'
+const META_DESCRIPTION = 'タピオカをつくるやつ'
+const META_KEYWORDS = ['タピオカ']
+const OG_IMAGE_WIDTH = 1200
+const OG_IMAGE_HEIGHZT = 630
+const FB_APPID = ''
+const TW_SITE = ''
+const TW_CREATOR = ''
 
-const genHtml = (id, url) => `
+const genHtml = (id, url, name) => `
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>${title}</title>
-    <meta name="description" content=${meta_description}>
-    <meta name="keywords" content=${meta_keywords.join(',')}>
+    <title>${TITLE}</title>
+    <meta name="description" content=${META_DESCRIPTION}>
+    <meta name="keywords" content=${META_KEYWORDS.join(',')}>
     <meta property="og:locale" content="ja_JP">
     <meta property="og:type" content="website">
     <meta property="og:url" content=${url}>
-    <meta property="og:title" content=${title}>
-    <meta property="og:site_name" content=${site_name}>
-    <meta property="og:description" content=${og_description}>
+    <meta property="og:title" content=${TITLE}>
+    <meta property="og:SITE_NAME" content=${SITE_NAME}>
+    <meta property="og:description" content=${name}>
     <meta property="og:image" content=${url}>
-    <meta property="og:image:width" content=${og_image_width}>
-    <meta property="og:image:height" content=${og_image_height}>
-    <meta property="fb:app_id" content=${fb_appid}>
+    <meta property="og:image:width" content=${OG_IMAGE_WIDTH}>
+    <meta property="og:image:height" content=${OG_IMAGE_HEIGHZT}>
+    <meta property="fb:app_id" content=${FB_APPID}>
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content=${title}>
-    <meta name="twitter:description" content=${tw_description}>
+    <meta name="twitter:title" content=${TITLE}>
+    <meta name="twitter:description" content=#${name}>
     <meta name="twitter:image" content=${url}>
-    <meta name="twitter:site" content=${tw_site}>
-    <meta name="twitter:creator" content=${tw_creator}>
+    <meta name="twitter:site" content=${TW_SITE}>
+    <meta name="twitter:creator" content=${TW_CREATOR}>
   </head>
   <body>
     <script>
@@ -62,7 +60,8 @@ app.get('/share/:id', async (req, res) => {
     res.status(404).send('404 Not Exist')
   } else {
     const url = doc.data().url
-    const html = genHtml(id, url)
+    const name = doc.data().name
+    const html = genHtml(id, url, name)
     res.set('cache-control', 'public, max-age=3600');
     res.send(html)
   }
